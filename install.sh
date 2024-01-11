0477c4275550ec2d26625994624c90aa327b557f
root <elliotkeavney@protonmail.com>
2024-01-11 15:34:19 +0000

    .

--- install.sh
+++ install.sh
@@ -44,14 +44,6 @@ read -p "Public IPv4 Address: " IPv4;
 printf "\nPlease enter IPv6 address of server, if no public IPv6 address, ::1 can be used.\n";
 read -p "Public IPv6 Address: " IPv6;
 
-# Install Certbot
-
-apt install snapd;
-snap install core;
-snap refresh core;
-snap install --classic certbot;  
-ln -s /snap/bin/certbot /usr/bin/certbot;
-
 # Check Let's Encrypt certificate directory exists and request
 # certificates if they do not exist.
 # Check FQDN has been input
